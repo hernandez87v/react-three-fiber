243f957459772f71abbff9ecf26c2c73a7c0b1f7
hernandez87v <hernandez87v@hotmail.com>
2020-10-04 18:19:19 -0700

    first commit

---
+++ src/fcm-config.js
@@ -0,0 +1,10 @@
+export default {
+  // Project Settings => Add Firebase to your web app
+  apiKey: "AIzaSyDrh90GhjpAGtAHVnOgjVTVMz_xxxxxxxx",
+  authDomain: "fcm-implement.firebaseapp.com",
+  databaseURL: "https://fcm-implement.firebaseio.com",
+  projectId: "fcm-implement",
+  storageBucket: "fcm-implement.appspot.com",
+  messagingSenderId: "80716xxxxxxx",
+  appId: "1:807163428649:web:xxxxxxxxxxxxxxxxxxxxxx"
+}
